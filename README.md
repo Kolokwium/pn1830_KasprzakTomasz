1e937271557cc86ff53df7cd0d21b9d2c001004e
cvcv <cxvxcv@sfds.pl>
2015-06-15 19:53:15 +0200

    To ja

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pn1830_KasprzakTomasz
+Tworzymy program wypisuj¹cy moje Imie i Nazwisko
---
+++ Tomasz/src/Tomasz.java
@@ -0,0 +1,5 @@
+public class Tomasz{
+  public static void main(String[] args){
+    System.out.print("Tomasz Kasprzak");
+  }
+}
